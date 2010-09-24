d409b33aa2d91c69f464b8b068051400726d5a62
Duncan Beevers <duncanbeevers@gmail.com>
2010-09-24 10:27:54 -0700

    All tests are executed with the test dir as pwd

--- test/sheller_test.rb
+++ test/sheller_test.rb
@@ -3,10 +3,17 @@ require File.join(File.dirname(__FILE__), 'test_helper')
 class ShellerTest < Test::Unit::TestCase
   TEST_DIR = File.join(File.dirname(__FILE__))
   
+  def setup
+    @original_dir = Dir.pwd
+    Dir.chdir(TEST_DIR)
+  end
+  
+  def teardown
+    Dir.chdir(@original_dir)
+  end
+  
   def test_command_no_arguments
-    Dir.chdir(TEST_DIR) do
-      assert_equal "turtle\n", Sheller.execute('./echo_turtle')[:STDOUT]
-    end
+    assert_equal "turtle\n", Sheller.execute('./echo_turtle')[:STDOUT]
   end
   
 end
